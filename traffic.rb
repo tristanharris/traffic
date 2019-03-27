6fd255f77abb13c62c85ffd5e674ff68583a2d14
Tristan Harris <tristan@sticktech.co.uk>
2019-03-27 18:03:12 +0000

    Extract angles

--- traffic.rb
+++ traffic.rb
@@ -1,32 +1,74 @@
 require 'tk'
 
+class Angle
+
+  def initialize(degrees)
+    @degrees = degrees
+  end
+
+  def degrees
+    @degrees
+  end
+
+  def radians
+    degrees * Math::PI / 180.0
+  end
+
+  def set(degrees)
+    @degrees = degrees
+  end
+
+  def rand!
+    @degrees = 360 * rand
+  end
+
+  def *(other)
+    Angle.new((degrees * other) % 360)
+  end
+
+  def +(other)
+    other = other.degrees if other.respond_to?(:degrees)
+    Angle.new((degrees + other) % 360)
+  end
+
+  def cos
+    Math.cos(radians)
+  end
+
+  def sin
+    Math.sin(radians)
+  end
+
+end
+
 class Car
 
   attr_reader :x, :y
 
   def initialize()
-    @direction = 2 * Math::PI * rand
+    @direction = Angle.new(0)
+    @direction.rand!
     @x = 100 * rand
     @y = 100 * rand
     @speed = 10
     @element = nil
-    @steeringAngle = 0
+    @steeringAngle = Angle.new(0)
     @targetSpeed = 20
     @scanRadius = 20
     @inTraffic = false
   end
 
   def tick(canvas, cars)
-    @x += Math.cos(@direction) * @speed / 10
-    @y += Math.sin(@direction) * @speed / 10
+    @x += @direction.cos * @speed / 10
+    @y += @direction.sin * @speed / 10
     if @x < 0 || @y < 0 || @x > canvas.winfo_width || @y > canvas.winfo_height
-      @direction = 2 * Math::PI * rand
+      @direction.rand!
       @x = 0 if @x < 0
       @y = 0 if @y < 0
       @x = canvas.winfo_width if @x > canvas.winfo_width
       @y = canvas.winfo_height if @y > canvas.winfo_height
     end
-    @direction = (@direction + (@steeringAngle / Math::PI)) % (2*Math::PI)
+    @direction += @steeringAngle
     @speed += 0.2 if (@speed < @targetSpeed)
     @speed -= 0.8 if (@speed > @targetSpeed)
     inTraffic = false
@@ -36,14 +78,14 @@ class Car
       next if distance > @scanRadius
       inTraffic = true
     end
-    @steeringAngle = 0.2 if inTraffic && !@inTraffic
-    @steeringAngle = 0 if !inTraffic
+    @steeringAngle.set(5) if inTraffic && !@inTraffic
+    @steeringAngle.set(0) if !inTraffic
     @inTraffic = inTraffic
   end
 
   def render(canvas)
     canvas.delete(@element) if @element
-    @element = TkcLine.new(canvas, @x, @y, @x + Math.cos(@direction) * @speed / 10 , @y + Math.sin(@direction) * @speed / 10, :arrow => 'last', :width => 1, :fill => @inTraffic ? :red : :yellow)
+    @element = TkcLine.new(canvas, @x, @y, @x + @direction.cos * @speed / 10 , @y + @direction.sin * @speed / 10, :arrow => 'last', :width => 1, :fill => @inTraffic ? :red : :yellow)
   end
 
 end
